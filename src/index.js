d023e9189b65d8288895c80d51ba97696cd470b4
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-06-02 20:15:13 +1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ const manager = new ShardingManager("src/engine.js", {
 	token: process.env.TOKEN,
 });
 
+console.log(" __  __     ______     __   __     __   __     ______    ")
+console.log("/\ \/ /    /\  __ \   /\ "-.\ \   /\ "-.\ \   /\  __ \   ")
+console.log("\ \  _"-.  \ \  __ \  \ \ \-.  \  \ \ \-.  \  \ \  __ \  ")
+console.log(" \ \_\ \_\  \ \_\ \_\  \ \_\\"\_\  \ \_\\"\_\  \ \_\ \_\ ")
+console.log("  \/_/\/_/   \/_/\/_/   \/_/ \/_/   \/_/ \/_/   \/_/\/_/ ")
+console.log("                                                         ")
+
 manager.on("shardCreate", (shard) => log("info", `Launched shard ${shard.id}`));
 manager.spawn();
