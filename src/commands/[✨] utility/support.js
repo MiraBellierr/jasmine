113f7f68220084a5e4540ba8e5e3f38ceef20251
MiraBellierr <danialzamri0908@gmail.com>
2022-06-08 00:54:53 +0800

    close issue #7 - Add support command

---
+++ src/commands/[✨] utility/support.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "support",
+	description: "Kanna bot support Discord server",
+	category: "[✨] utility",
+	run: async (client, message) => {
+		message.reply("Our support server:\nhttps://discord.gg/NcPeGuNEdc");
+	},
+};
