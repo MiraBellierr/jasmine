df414fd10bfc3ae18689444577a6f330626d76ce
MiraBellierr <danialzamri0908@gmail.com>
2022-06-09 16:24:25 +0800

    add botinfo command (close #9)

---
+++ src/commands/[📚] info/botinfo.js
@@ -0,0 +1,71 @@
+const Discord = require("discord.js");
+const Util = require("../../utils/Util");
+const osu = require("node-os-utils");
+const si = require("systeminformation");
+const sqlite = require("../../../node_modules/sqlite3/package.json");
+
+const cpu = osu.cpu;
+const drive = osu.drive;
+const mem = osu.mem;
+const os = osu.os;
+
+module.exports = {
+	name: "botinfo",
+	aliases: ["bot"],
+	category: "[📚] info",
+	description: "Shows bot information",
+	run: async (client, message) => {
+		const m = await message.channel.send("*Please wait...*");
+		const clientApplication = await client.application.fetch();
+		const owner = clientApplication.owner.tag;
+
+		const cpuCount = cpu.count();
+		const cpuUsagePercentage = await cpu.usage();
+		const driveInfo = await drive.info();
+		const memInfo = await mem.info();
+		const osInfo = await os.oos();
+		const processor = await si.cpu();
+
+		let totalSeconds = client.uptime / 1000;
+		const days = Math.floor(totalSeconds / 86400);
+		const hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		const minutes = Math.floor(totalSeconds / 60);
+		const uptime = `${days} days, ${hours} hours, ${minutes} minutes`;
+
+		m.delete();
+
+		const embed = new Discord.MessageEmbed()
+			.setAuthor({ name: "Bot Information" })
+			.setThumbnail(client.user.avatarURL())
+			.setColor("#DA70D6")
+			.setDescription(
+				`**• Developer:** ${owner}\n**• Tag:** ${
+					client.user.tag
+				}\n**• Cached Members:** ${client.users.cache.size.toLocaleString()}\n**• Total Members:** ${client.guilds.cache
+					.map((guild) => guild.memberCount)
+					.reduce((accumulator, currentValue) => accumulator + currentValue)
+					.toLocaleString()}\n**• Total Servers:** ${client.guilds.cache.size.toLocaleString()}\n**• Total Shards:** ${client.shard.count.toLocaleString()}\n**• Total Channels:** ${client.channels.cache.size.toLocaleString()}\n**• Total Emojis:** ${client.emojis.cache.size.toLocaleString()}\n**• Created At:** ${formatDate(
+					client.user.createdAt
+				)}\n**• Library:** Discord.js v${
+					Discord.version
+				}\n**• Database:** SQlite3 v${sqlite.version}\n**• JRE:** Node ${
+					process.version
+				}\n**• Websocket Status:** ${
+					client.ws.status
+				}\n**• Websocket Ping:** ${client.ws.ping.toLocaleString()}ms\n**• CPU Count:** ${cpuCount}\n**• CPU Usage:** ${cpuUsagePercentage.toFixed(
+					2
+				)}%\n**• Drive Usage:** ${driveInfo.usedGb}GB (${
+					driveInfo.usedPercentage
+				}%)\n**• Memory Usage:** ${(memInfo.usedMemMb / 1000).toFixed(2)}GB (${(
+					100 - memInfo.freeMemPercentage
+				).toFixed(2)}%)\n**• Operating System:** ${osInfo}\n**• Processor:** ${
+					processor.manufacturer
+				} ${processor.brand}\n**• Ready At:** ${formatDate(
+					client.readyAt
+				)}\n**• Uptime:** ${uptime}`
+			);
+
+		message.reply({ embeds: [embed] });
+	},
+};
