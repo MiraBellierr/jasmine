2cd4a4f2812d0f965ad83c10f08e1be8be869757
MiraBellierr <danialzamri0908@gmail.com>
2022-06-12 15:57:49 +0800

    improve servericon command

--- src/commands/[📚] info/servericon.js
+++ src/commands/[📚] info/servericon.js
@@ -12,21 +12,74 @@ module.exports = {
 
 		const pages = [];
 
-		const embed = new Discord.MessageEmbed()
+		const iconEmbed = new Discord.MessageEmbed()
 			.setAuthor({
 				name: message.author.username,
 				iconURL: message.author.displayAvatarURL({ dynamic: true }),
 			})
 			.setTitle("Server Icon")
 			.setColor("#CD1C6C")
+			.setDescription(
+				`[webp](${message.guild.iconURL({
+					format: "webp",
+					size: 4096,
+				})}) | [png](${message.guild.iconURL({
+					format: "png",
+					size: 4096,
+				})}) | [jpg](${message.guild.iconURL({
+					format: "jpg",
+					size: 4096,
+				})}) | [jpeg](${message.guild.iconURL({
+					format: "jpeg",
+					size: 4096,
+				})}) | [gif](${message.guild.iconURL({
+					dynamic: true,
+					size: 4096,
+					format: "gif",
+				})})`
+			)
 			.setImage(message.guild.iconURL({ dynamic: true, size: 4096 }))
 			.setTimestamp()
 			.setFooter({ text: client.user.tag, iconURL: client.user.avatarURL() });
 
-		pages.push(embed);
+		pages.push(iconEmbed);
+
+		if (message.guild.splashURL()) {
+			const splashEmbed = new Discord.MessageEmbed()
+				.setAuthor({
+					name: message.author.username,
+					iconURL: message.author.displayAvatarURL({ dynamic: true }),
+				})
+				.setTitle("Server Splash")
+				.setColor("#CD1C6C")
+				.setImage(message.guild.splashURL({ dynamic: true, size: 4096 }))
+				.setDescription(
+					`[webp](${message.guild.splashURL({
+						format: "webp",
+						size: 4096,
+					})}) | [png](${message.guild.splashURL({
+						format: "png",
+						size: 4096,
+					})}) | [jpg](${message.guild.splashURL({
+						format: "jpg",
+						size: 4096,
+					})}) | [jpeg](${message.guild.splashURL({
+						format: "jpeg",
+						size: 4096,
+					})}) | [gif](${message.guild.splashURL({
+						dynamic: true,
+						size: 4096,
+						format: "gif",
+					})})`
+				)
+				.setTimestamp()
+				.setFooter({ text: client.user.tag, iconURL: client.user.avatarURL() });
+
+			pages.push(splashEmbed);
+		}
 
 		if (message.guild.bannerURL()) {
-			const banner = new Discord.MessageEmbed()
+			const bannerEmbed = new Discord.MessageEmbed()
 				.setAuthor({
 					name: message.author.username,
 					iconURL: message.author.displayAvatarURL({ dynamic: true }),
@@ -34,10 +87,65 @@ module.exports = {
 				.setTitle("Server Banner")
 				.setColor("#CD1C6C")
 				.setImage(message.guild.bannerURL({ dynamic: true, size: 4096 }))
+				.setDescription(
+					`[webp](${message.guild.bannerURL({
+						format: "webp",
+						size: 4096,
+					})}) | [png](${message.guild.bannerURL({
+						format: "png",
+						size: 4096,
+					})}) | [jpg](${message.guild.bannerURL({
+						format: "jpg",
+						size: 4096,
+					})}) | [jpeg](${message.guild.bannerURL({
+						format: "jpeg",
+						size: 4096,
+					})}) | [gif](${message.guild.bannerURL({
+						dynamic: true,
+						size: 4096,
+						format: "gif",
+					})})`
+				)
 				.setTimestamp()
 				.setFooter({ text: client.user.tag, iconURL: client.user.avatarURL() });
 
-			pages.push(banner);
+			pages.push(bannerEmbed);
+		}
+
+		if (message.guild.discoverySplashURL()) {
+			const discoverySplashEmbed = new Discord.MessageEmbed()
+				.setAuthor({
+					name: message.author.username,
+					iconURL: message.author.displayAvatarURL({ dynamic: true }),
+				})
+				.setTitle("Server Splash (Discovery)")
+				.setColor("#CD1C6C")
+				.setImage(
+					message.guild.discoverySplashURL({ dynamic: true, size: 4096 })
+				)
+				.setDescription(
+					`[webp](${message.guild.discoverySplashURL({
+						format: "webp",
+						size: 4096,
+					})}) | [png](${message.guild.discoverySplashURL({
+						format: "png",
+						size: 4096,
+					})}) | [jpg](${message.guild.discoverySplashURL({
+						format: "jpg",
+						size: 4096,
+					})}) | [jpeg](${message.guild.discoverySplashURL({
+						format: "jpeg",
+						size: 4096,
+					})}) | [gif](${message.guild.discoverySplashURL({
+						dynamic: true,
+						size: 4096,
+						format: "gif",
+					})})`
+				)
+				.setTimestamp()
+				.setFooter({ text: client.user.tag, iconURL: client.user.avatarURL() });
+
+			pages.push(discoverySplashEmbed);
 		}
 
 		new Paginate.Paginate(client, message, pages).init();
