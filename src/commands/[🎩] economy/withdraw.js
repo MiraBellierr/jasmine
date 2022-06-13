d779e7376b2a728caf73b435b39c3dc1707e3028
MiraBellierr <danialzamri0908@gmail.com>
2022-06-13 19:36:22 +0800

    add withdraw command

---
+++ src/commands/[🎩] economy/withdraw.js
@@ -0,0 +1,74 @@
+const schemas = require("../../database/schemas");
+const Economy = require("../../utils/Economy");
+const Error = require("../../utils/Error");
+const constants = require("../../utils/constants");
+
+module.exports = {
+	name: "withdraw",
+	aliases: ["with"],
+	description: "Withdraw your coins from a bank to your pocket",
+	category: "[🎩] economy",
+	usage: "<amount | all | max>",
+	run: async (client, message, args) => {
+		if (!args.length)
+			return new Error(module.exports, client, message).argsError();
+
+		const coins = await new Economy(client).getCoins(message.author);
+		const currentMaxDeposit = coins.get("maxDeposit");
+		const currentBank = coins.get("bank");
+		const currentPocket = coins.get("pocket");
+		const currentSpace = currentMaxDeposit - currentBank;
+
+		if (args[0] === "max" || args[0] === "all") {
+			if (currentBank < 1)
+				return message.reply(
+					`**${message.author.username}**, you don't have any coins in your bank to be withdrew`
+				);
+
+			schemas.coins().update(
+				{
+					pocket: currentPocket + currentBank,
+					bank: 0,
+				},
+				{ where: { userID: message.author.id } }
+			);
+
+			message.reply(
+				`**${message.author.username}**, you have withdrew ${
+					constants.coins.emoji
+				} ${currentBank.toLocaleString()} from your bank`
+			);
+		} else {
+			if (isNaN(args[0]))
+				return message.reply(
+					`**${message.author.username}**, you have to withdraw a real amount of coins from your bank`
+				);
+
+			if (args[0] < 1)
+				return message.reply(
+					`**${message.author.username}**, you have to withdraw positive amount of coins only from your bank`
+				);
+
+			if (currentBank < args[0])
+				return message.reply(
+					`**${message.author.username}**, you dont have that much coins in your bank to be withdrew`
+				);
+
+			const amount = parseInt(args[0]);
+
+			schemas.coins().update(
+				{
+					pocket: currentPocket + amount,
+					bank: currentBank - amount,
+				},
+				{ where: { userID: message.author.id } }
+			);
+
+			message.reply(
+				`**${message.author.username}**, you have withdrew ${
+					constants.coins.emoji
+				} ${amount.toLocaleString()} from your bank`
+			);
+		}
+	},
+};
