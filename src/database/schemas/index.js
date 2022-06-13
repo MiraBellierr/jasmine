3e6ebfff727c6944963ab90771f5cc407d4f179b
MiraBellierr <danialzamri0908@gmail.com>
2022-06-13 18:42:11 +0800

    add new timer table

--- src/database/schemas/index.js
+++ src/database/schemas/index.js
@@ -3,5 +3,13 @@ const leaveMessage = require("./leaveMessage");
 const welcomeMessage = require("./welcomeMessage");
 const starboard = require("./starboard");
 const coins = require("./coins");
+const timer = require("./timer");
 
-module.exports = { guild, welcomeMessage, leaveMessage, starboard, coins };
+module.exports = {
+	guild,
+	welcomeMessage,
+	leaveMessage,
+	starboard,
+	coins,
+	timer,
+};
