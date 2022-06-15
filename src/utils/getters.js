977f66e33109839033a1999d5492870ea3c1fb01
MiraBellierr <danialzamri0908@gmail.com>
2022-06-15 14:45:34 +0800

    fix: variables

--- src/utils/getters.js
+++ src/utils/getters.js
@@ -4,16 +4,19 @@ const getUserFromArguments = async (message, arguments) => {
 	if (message.mentions.users.first()) return message.mentions.users.first();
 
 	if (!Number.isNaN(Number(userToFind))) {
-		const fetched = await message.client.users.fetch(userToFind).catch(() => null);
+		const fetched = await message.client.users
+			.fetch(userToFind)
+			.catch(() => null);
 
 		if (fetched) return fetched;
 	}
 
-	return message.client.users.cache.find(target =>
-		target.username.toLowerCase().includes(toFind) ||
-		target.tag.toLowerCase().includes(toFind)
+	return message.client.users.cache.find(
+		(target) =>
+			target.username.toLowerCase().includes(userToFind) ||
+			target.tag.toLowerCase().includes(userToFind)
 	);
-}
+};
 
 const getMemberFromArguments = async (message, arguments) => {
 	const memberToFind = arguments.toLowerCase();
@@ -21,31 +24,37 @@ const getMemberFromArguments = async (message, arguments) => {
 	if (message.mentions.members.first()) return message.mentions.members.first();
 
 	if (!Number.isNaN(Number(memberToFind))) {
-		const fetched = await message.guild.members.fetch(memberToFind).catch(() => null);
+		const fetched = await message.guild.members
+			.fetch(memberToFind)
+			.catch(() => null);
 
 		if (fetched) return fetched;
 	}
 
-	return message.guild.members.cache.find(target =>
-		target.displayName.toLowerCase().includes(memberToFind) ||
-		target.user.tag.toLowerCase().includes(memberToFind)
+	return message.guild.members.cache.find(
+		(target) =>
+			target.displayName.toLowerCase().includes(memberToFind) ||
+			target.user.tag.toLowerCase().includes(memberToFind)
 	);
-}
+};
 
 const getChannelFromArguments = async (message, arguments) => {
 	const channelToFind = arguments.toLowerCase();
 
-	if (message.mentions.channels.first()) return message.mentions.channels.first();
+	if (message.mentions.channels.first())
+		return message.mentions.channels.first();
 
 	if (!Number.isNaN(Number(channelToFind))) {
-		const fetched = await message.guild.channels.fetch(channelToFind).catch(() => null);
+		const fetched = await message.guild.channels
+			.fetch(channelToFind)
+			.catch(() => null);
 		if (fetched) return fetched;
 	}
 
 	return message.guild.channels.cache.find((channel) =>
 		channel.name.toLowerCase().includes(channelToFind)
 	);
-}
+};
 
 const getRoleFromArguments = async (message, arguments) => {
 	const roleToFind = arguments.toLowerCase();
@@ -53,11 +62,20 @@ const getRoleFromArguments = async (message, arguments) => {
 	if (message.mentions.roles.first()) return message.mentions.roles.first();
 
 	if (!Number.isNaN(Number(roleToFind))) {
-		const fetched = await message.guild.roles.fetch(roleToFind).catch(() => null);
+		const fetched = await message.guild.roles
+			.fetch(roleToFind)
+			.catch(() => null);
 		if (fetched) return fetched;
 	}
 
-	return message.guild.roles.cache.find((role) => role.name.toLowerCase().includes(roleToFind));
-}
+	return message.guild.roles.cache.find((role) =>
+		role.name.toLowerCase().includes(roleToFind)
+	);
+};
 
-module.exports = { getUserFromArguments, getMemberFromArguments, getChannelFromArguments, getRoleFromArguments };
+module.exports = {
+	getUserFromArguments,
+	getMemberFromArguments,
+	getChannelFromArguments,
+	getRoleFromArguments,
+};
