c4d53f9ae64f050277f759a53535c946ca3eb9d4
MiraBellierr <75848@siswa.unimas.my>
2022-07-03 10:08:37 +0800

    🔧 add: purge bot only messages option

--- src/commands/[🛠] moderation/purge.js
+++ src/commands/[🛠] moderation/purge.js
@@ -4,61 +4,105 @@ module.exports = {
 	name: "purge",
 	category: "[🛠] moderation",
 	description: "delete some messages",
-	usage: "<count>",
+	clientPermissions: "MANAGE_MESSAGES",
+	memberPermissions: "MANAGE_MESSAGES",
+	usage: "[bot] <count>",
 	run: async (client, message, args) => {
-		if (!message.guild.me.permissions.has("MANAGE_MESSAGES"))
-			return message.channel.send(
-				"I do not have `MANAGE_MESSAGES` permission to be able to continue this command"
-			);
+		if (message.deletable) message.delete();
 
-		if (!message.member.permissions.has("MANAGE_MESSAGES"))
-			return message.channel.send(
-				"You don't have `MANAGE_MESSAGES` permission to use this command"
-			);
+		if (!isNaN(args[0])) {
+			let messageCount = args[0];
 
-		let messageCount = args[0];
+			if (!messageCount || isNaN(messageCount))
+				return argsError(module.exports, client, message);
 
-		if (!messageCount || isNaN(messageCount))
-			return message.channel.send(argsError(module.exports, client, message));
-
-		messageCount++;
-
-		if (messageCount > 100) {
-			messageCount = 100;
-		}
-
-		const fetch = await message.channel.messages.fetch({ limit: messageCount });
-		const deletedMessages = (await message.channel.bulkDelete(fetch, true)).map(
-			(m) => m
-		);
-
-		deletedMessages.shift();
-
-		const results = {};
-
-		for (const deleted of deletedMessages) {
-			const user = `${deleted.author.tag}`;
-
-			if (!user) {
-				continue;
-			} else if (!results[user]) {
-				results[user] = 0;
+			if (messageCount > 100) {
+				messageCount = 100;
 			}
 
-			results[user]++;
+			const fetch = await message.channel.messages.fetch({
+				limit: parseInt(messageCount),
+			});
+
+			const deletedMessages = (
+				await message.channel.bulkDelete(fetch, true)
+			).map((m) => m);
+
+			const results = {};
+
+			for (const deleted of deletedMessages) {
+				if (!deleted) continue;
+
+				const user = `${deleted.author.tag}`;
+
+				if (!results[user]) {
+					results[user] = 0;
+				}
+
+				results[user]++;
+			}
+
+			const userMessageMap = Object.entries(results);
+			const formed = `${deletedMessages.length} message${
+				deletedMessages.length > 1 ? "s" : ""
+			} were removed.\n\n${userMessageMap
+				.map(([user, messages]) => `**${user}**: ${messages}`)
+				.join("\n")}`;
+
+			message.channel.send(formed).then((m) => {
+				setTimeout(() => {
+					m.delete();
+				}, 5000);
+			});
+		} else if (args[0] === "bot") {
+			let messageCount = args[1];
+
+			if (!messageCount || isNaN(messageCount))
+				return argsError(module.exports, client, message);
+
+			if (messageCount > 100) {
+				messageCount = 100;
+			}
+
+			let fetch = await message.channel.messages.fetch();
+
+			fetch = fetch
+				.filter((m) => m.author.bot)
+				.toJSON()
+				.slice(0, parseInt(messageCount));
+
+			const deletedMessages = (
+				await message.channel.bulkDelete(fetch, true)
+			).map((m) => m);
+
+			const results = {};
+
+			for (const deleted of deletedMessages) {
+				if (!deleted) continue;
+
+				const user = `${deleted.author.tag}`;
+
+				if (!results[user]) {
+					results[user] = 0;
+				}
+
+				results[user]++;
+			}
+
+			const userMessageMap = Object.entries(results);
+			const formed = `${deletedMessages.length} message${
+				deletedMessages.length > 1 ? "s" : ""
+			} were removed.\n\n${userMessageMap
+				.map(([user, messages]) => `**${user}**: ${messages}`)
+				.join("\n")}`;
+
+			message.channel.send(formed).then((m) => {
+				setTimeout(() => {
+					m.delete();
+				}, 5000);
+			});
+		} else {
+			return argsError(module.exports, client, message);
 		}
-
-		const userMessageMap = Object.entries(results);
-		const formed = `${deletedMessages.length} message${
-			deletedMessages.length > 1 ? "s" : ""
-		} were removed.\n\n${userMessageMap
-			.map(([user, messages]) => `**${user}**: ${messages}`)
-			.join("\n")}`;
-
-		message.channel.send(formed).then((m) => {
-			setTimeout(() => {
-				m.delete();
-			}, 5000);
-		});
 	},
 };
