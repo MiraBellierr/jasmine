e67c9cb8332a28d9561461f3297b31502f715cca
MiraBellierr <75848@siswa.unimas.my>
2022-07-20 21:34:36 +0800

    add message edit event for logging

---
+++ src/events/messageUpdate/logging.js
@@ -0,0 +1,45 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (client, oldMessage, newMessage) => {
+	const logging = client.loggings.get(newMessage.guild.id);
+
+	if (!logging || !logging.defaultLogChannel) return;
+
+	if (!logging.messageEdit) return;
+
+	if (logging.ignoredChannels) {
+		const ignoredChannels = logging.ignoredChannels.split("|");
+
+		if (ignoredChannels.includes(newMessage.channel.id)) {
+			return;
+		}
+	}
+
+	if (oldMessage.cleanContent !== newMessage.cleanContent) {
+		const embed = new EmbedBuilder()
+			.setAuthor({
+				name: `Message Edited`,
+				iconURL: newMessage.author.displayAvatarURL(),
+			})
+			.setColor("#CD1C6C")
+			.setDescription(`**Member:** ${newMessage.member}`)
+			.addFields([
+				{
+					name: "Before",
+					value: `${oldMessage.cleanContent}`,
+				},
+				{
+					name: "After",
+					value: `${newMessage.cleanContent}`,
+				},
+			])
+			.setFooter({ text: `messageid: ${newMessage.id}` })
+			.setTimestamp();
+
+		const logChannel = await newMessage.guild.channels.fetch(
+			logging.defaultLogChannel
+		);
+
+		logChannel.send({ embeds: [embed] });
+	}
+};
