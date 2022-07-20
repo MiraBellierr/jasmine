1d5dfa96b22c30fdb1d73c18e9f9dc9de1fd02bb
MiraBellierr <75848@siswa.unimas.my>
2022-07-20 21:33:27 +0800

    add ban event for logging

---
+++ src/events/guildBanAdd.js/logging.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (client, ban) => {
+	const logging = client.loggings.get(ban.guild.id);
+
+	if (!logging || !logging.defaultLogChannel) return;
+
+	if (!logging.memberBans) return;
+
+	await ban.fetch();
+
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: "Member Banned",
+			iconURL: ban.guild.iconURL(),
+		})
+		.setColor("#CD1C6C")
+		.setDescription(`**Member:** ${ban.user.tag}\n**Reason:** ${ban.reason}`)
+		.setFooter({ text: `memberid: ${ban.user.id}` })
+		.setTimestamp();
+
+	let logChannel;
+
+	if (logging.memberLogChannel) {
+		logChannel = await ban.guild.channels.fetch(logging.serverLogChannel);
+	} else {
+		logChannel = await ban.guild.channels.fetch(logging.defaultLogChannel);
+	}
+
+	logChannel.send({ embeds: [embed] });
+};
