5930c395746704b8815fe87aacb5eddd4a0689de
MiraBellierr <75848@siswa.unimas.my>
2022-07-20 21:33:48 +0800

    add joinleave event for logging

---
+++ src/events/guildMemberRemove/logging.js
@@ -0,0 +1,29 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (client, member) => {
+	const logging = client.loggings.get(member.guild.id);
+
+	if (!logging || !logging.defaultLogChannel) return;
+
+	if (!logging.memberLeave) return;
+
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: "Member Left",
+			iconURL: member.user.displayAvatarURL(),
+		})
+		.setColor("#CD1C6C")
+		.setDescription(`**Member:** ${member.user.tag}`)
+		.setFooter({ text: `Memberid: ${member.id}` })
+		.setTimestamp();
+
+	let logChannel;
+
+	if (logging.joinLeaveLogChannel) {
+		logChannel = await member.guild.channels.fetch(logging.joinLeaveLogChannel);
+	} else {
+		logChannel = await member.guild.channels.fetch(logging.defaultLogChannel);
+	}
+
+	logChannel.send({ embeds: [embed] });
+};
