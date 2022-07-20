126da28501c73a58be880f71553136bec176da63
MiraBellierr <75848@siswa.unimas.my>
2022-07-20 14:19:01 +0800

    add roleDelete logging event

---
+++ src/events/roleDelete/logging.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (client, role) => {
+	const logging = client.loggings.get(role.guild.id);
+
+	if (!logging || !logging.roleDeletion) return;
+
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: "Role Deleted",
+			iconURL: role.guild.iconURL(),
+		})
+		.setColor("#CD1C6C")
+		.setDescription(`Role: ${role.name}`)
+		.setFooter({ text: `roleid: ${role.id}` })
+		.setTimestamp();
+
+	let logChannel;
+
+	if (logging.serverLogChannel) {
+		logChannel = await role.guild.channels.fetch(logging.serverLogChannel);
+	} else {
+		logChannel = await role.guild.channels.fetch(logging.defaultLogChannel);
+	}
+
+	logChannel.send({ embeds: [embed] });
+};
