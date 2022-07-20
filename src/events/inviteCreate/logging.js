fcfba62a3cfd90f89d7ad46a90380985edfb6d42
MiraBellierr <75848@siswa.unimas.my>
2022-07-20 21:34:11 +0800

    add discord invite event for logging

---
+++ src/events/inviteCreate/logging.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (client, invite) => {
+	const logging = client.loggings.get(invite.guild.id);
+
+	if (!logging || !logging.defaultLogChannel) return;
+
+	if (!logging.discordInvites) return;
+
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: "Invite Created",
+			iconURL: invite.inviter.displayAvatarURL(),
+		})
+		.setColor("#CD1C6C")
+		.setDescription(
+			`**Invite Code:** ${invite.code}\n**Inviter:** ${invite.inviter}`
+		)
+		.setFooter({ text: `inviterid: ${invite.inviterId}` })
+		.setTimestamp();
+
+	let logChannel;
+
+	if (logging.serverLogChannel) {
+		logChannel = await invite.guild.channels.fetch(logging.serverLogChannel);
+	} else {
+		logChannel = await invite.guild.channels.fetch(logging.defaultLogChannel);
+	}
+
+	logChannel.send({ embeds: [embed] });
+};
---
+++ src/events/inviteDelete/logging.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (client, invite) => {
+	const logging = client.loggings.get(invite.guild.id);
+
+	if (!logging || !logging.defaultLogChannel) return;
+
+	if (!logging.discordInvites) return;
+
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: "Invite Deleted",
+			iconURL: invite.guild.iconURL(),
+		})
+		.setColor("#CD1C6C")
+		.setDescription(`**Invite Code:** ${invite.code}`)
+		.setTimestamp();
+
+	let logChannel;
+
+	if (logging.serverLogChannel) {
+		logChannel = await invite.guild.channels.fetch(logging.serverLogChannel);
+	} else {
+		logChannel = await invite.guild.channels.fetch(logging.defaultLogChannel);
+	}
+
+	logChannel.send({ embeds: [embed] });
+};
