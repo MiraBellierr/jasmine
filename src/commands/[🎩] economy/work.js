412dde44ac27d0512c653b24ce3f29c144f1a719
MiraBellierr <75848@siswa.unimas.my>
2022-07-25 16:04:10 +0800

    refactor statement

--- src/commands/[🎩] economy/work.js
+++ src/commands/[🎩] economy/work.js
@@ -18,51 +18,50 @@ module.exports = {
 		const coins = await economy.getCoins(message.author);
 		const timer = await economy.getCooldown(message.author, "work", 3.6e6);
 
-		if (timer) {
-			message.reply(
+		if (timer)
+			return message.reply(
 				`**${message.author.username}**, Please wait **${timer.minutes}m ${timer.seconds}s** before working again.`
 			);
-		} else {
-			await schemas.timer().update(
+
+		await schemas.timer().update(
+			{
+				work: Date.now(),
+			},
+			{ where: { userID: message.author.id } }
+		);
+
+		const word = words[Math.floor(Math.random() * words.length)];
+
+		message.channel.send(
+			`**${message.author.username}**\nRetype this following phrase:\n\`${word}\``
+		);
+
+		const input = await message.channel.awaitMessages({
+			filter: (m) => m.author.id === message.author.id,
+			max: 1,
+			time: 30000,
+		});
+
+		if (!input.size) {
+			return message.channel.send("Time is up! You lost the job.");
+		}
+
+		if (input.first().content.toLowerCase() === word.toLowerCase()) {
+			const gain = Math.floor(Math.random() * 100);
+			const current = coins.get("pocket");
+
+			schemas.coins().update(
 				{
-					work: Date.now(),
+					pocket: current + gain,
 				},
 				{ where: { userID: message.author.id } }
 			);
 
-			const word = words[Math.floor(Math.random() * words.length)];
+			const amount = `${constants.coins.emoji} ${gain}`;
 
-			message.channel.send(
-				`**${message.author.username}**\nRetype this following phrase:\n\`${word}\``
-			);
-
-			const input = await message.channel.awaitMessages({
-				filter: (m) => m.author.id === message.author.id,
-				max: 1,
-				time: 30000,
-			});
-
-			if (!input.size) {
-				return message.channel.send("Time is up! You lost the job.");
-			}
-
-			if (input.first().content.toLowerCase() === word.toLowerCase()) {
-				const gain = Math.floor(Math.random() * 100);
-				const current = coins.get("pocket");
-
-				schemas.coins().update(
-					{
-						pocket: current + gain,
-					},
-					{ where: { userID: message.author.id } }
-				);
-
-				const amount = `${constants.coins.emoji} ${gain}`;
-
-				message.reply(economy.getWork(amount));
-			} else {
-				message.channel.send("Poor effort. You lost the job.");
-			}
+			message.reply(economy.getWork(amount));
+		} else {
+			message.channel.send("Poor effort. You lost the job.");
 		}
 	},
 };
