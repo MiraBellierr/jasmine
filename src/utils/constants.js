dc06ab6430ae32c1964fa27ec3628a3583caee80
MiraBellierr <75848@siswa.unimas.my>
2022-07-29 14:52:23 +0800

    add emoji constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -53,3 +53,34 @@ module.exports.macs = {
 	isCute: true,
 	id: "724552867662528513",
 };
+
+module.exports.assets = {
+	class: {
+		path: "/assets/rpg-icons/167.png",
+		emoji: "<:class:1002186813877801052>",
+	},
+	xp: {
+		path: "/assets/rpg-icons/6.png",
+		emoji: "<:xp:1002188020746821643>",
+	},
+	hp: {
+		path: "/assets/rpg-icons/7.png",
+		emoji: "<:hp:1002188539129245746>",
+	},
+	str: {
+		path: "/assets/rpg-icons/16.png",
+		emoji: "<:str:1002190036097302618>",
+	},
+	agl: {
+		path: "/assets/rpg-icons/36.png",
+		emoji: "<:agl:1002191334335062117>",
+	},
+	att: {
+		path: "/assets/rpg-icons/64.png",
+		emoji: "<:att:1002191699310825582>",
+	},
+	def: {
+		path: "/assets/rpg-icons/65.png",
+		emoji: "<:def:1002192083387416626>",
+	},
+};
