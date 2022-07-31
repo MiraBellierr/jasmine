54aef25767fcfd8cdd1cd13da9d141fcb37d86d8
MiraBellierr <75848@siswa.unimas.my>
2022-07-31 19:39:35 +0800

    implement battle system with equipment

--- src/utils/rpg/battle/battle.js
+++ src/utils/rpg/battle/battle.js
@@ -5,6 +5,7 @@ const { getProgBar } = require("../../../utils/utils");
 const set = new Set();
 const { setTimeout } = require("timers/promises");
 const classes = require("../../../database/json/classes.json");
+const equipments = require("../../../database/json/equipments.json");
 
 class Battle {
 	constructor(message, user) {
@@ -30,9 +31,13 @@ class Battle {
 		let randomCharacter =
 			allCharacter[Math.floor(Math.random() * allCharacter.length)];
 
-		while (randomCharacter.get("userID") === this.user.id) {
-			randomCharacter =
-				allCharacter[Math.floor(Math.random() * allCharacter.length)];
+		if (allCharacter.length > 1) {
+			while (randomCharacter.get("userID") === this.user.id) {
+				randomCharacter =
+					allCharacter[Math.floor(Math.random() * allCharacter.length)];
+			}
+		} else {
+			randomCharacter = allCharacter[0];
 		}
 
 		return randomCharacter.toJSON();
@@ -50,8 +55,17 @@ class Battle {
 		set.add(this.user.id);
 
 		this.opponent = await this.getRandomOpponent();
+
 		this.character.maxHp = this.character.hp;
 		this.opponent.maxHp = this.opponent.hp;
+
+		this.character.crit = 0;
+		this.opponent.crit = 0;
+
+		this.opponent.weight = 0;
+		this.character.weight = 0;
+
+		await this.registerEquipments();
 
 		if (this.character.agl === this.opponent.agl) {
 			this.character.agl += Math.random();
@@ -183,7 +197,7 @@ class Battle {
 
 		while (character.xp > attr.xpNeeded) {
 			attr.level++;
-			attr.xpNeeded += 10;
+			attr.xpNeeded *= 2;
 
 			hpGain += Math.floor(character.sta / 4) + 1;
 			strGain += Math.floor(Math.random() * 2) + 1;
@@ -232,6 +246,100 @@ class Battle {
 		return xpGain;
 	}
 
+	async registerEquipments() {
+		const playerEquipments = JSON.parse(this.character.equipments);
+		const opponentEquipments = JSON.parse(this.opponent.equipments);
+		// weapons
+		if (playerEquipments.weapons.equipped) {
+			const attr = equipments.weapons[playerEquipments.weapons.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.character[key] += attr[key];
+			});
+		}
+		if (opponentEquipments.weapons.equipped) {
+			const attr = equipments.weapons[opponentEquipments.weapons.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.opponent[key] += attr[key];
+			});
+		}
+		// shields
+		if (playerEquipments.shields.equipped) {
+			const attr = equipments.shields[playerEquipments.shields.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.character[key] += attr[key];
+			});
+		}
+		if (opponentEquipments.shields.equipped) {
+			const attr = equipments.shields[opponentEquipments.shields.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.opponent[key] += attr[key];
+			});
+		}
+		// helmet
+		if (playerEquipments.helmet.equipped) {
+			const attr = equipments.helmet[playerEquipments.helmet.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.character[key] += attr[key];
+			});
+			if (equipments.helmet[playerEquipments.helmet.equipped].weight) {
+				this.character.weight +=
+					equipments.helmet[playerEquipments.helmet.equipped].weight;
+			}
+		}
+		if (opponentEquipments.helmet.equipped) {
+			const attr = equipments.helmet[opponentEquipments.helmet.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.opponent[key] += attr[key];
+			});
+			if (equipments.helmet[opponentEquipments.helmet.equipped].weight) {
+				this.opponent.weight +=
+					equipments.helmet[opponentEquipments.helmet.equipped].weight;
+			}
+		}
+		// armor
+		if (playerEquipments.armor.equipped) {
+			const attr = equipments.armor[playerEquipments.armor.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.character[key] += attr[key];
+			});
+			if (equipments.armor[playerEquipments.armor.equipped].weight) {
+				this.character.weight +=
+					equipments.armor[playerEquipments.armor.equipped].weight;
+			}
+		}
+		if (opponentEquipments.armor.equipped) {
+			const attr = equipments.armor[opponentEquipments.armor.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.opponent[key] += attr[key];
+			});
+			if (equipments.armor[opponentEquipments.armor.equipped].weight) {
+				this.opponent.weight +=
+					equipments.armor[opponentEquipments.armor.equipped].weight;
+			}
+		}
+		// gloves
+		if (playerEquipments.gloves.equipped) {
+			const attr = equipments.gloves[playerEquipments.gloves.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.character[key] += attr[key];
+			});
+			if (equipments.gloves[playerEquipments.gloves.equipped].weight) {
+				this.character.weight +=
+					equipments.gloves[playerEquipments.gloves.equipped].weight;
+			}
+		}
+		if (opponentEquipments.gloves.equipped) {
+			const attr = equipments.gloves[opponentEquipments.gloves.equipped].attr;
+			Object.keys(attr).forEach((key) => {
+				this.opponent[key] += attr[key];
+			});
+			if (equipments.gloves[opponentEquipments.gloves.equipped].weight) {
+				this.opponent.weight +=
+					equipments.gloves[opponentEquipments.gloves.equipped].weight;
+			}
+		}
+	}
+
 	playerAttack() {
 		const acc = Math.floor(
 			(this.character.acc / 100) *
@@ -246,15 +354,25 @@ class Battle {
 				(this.character.att + this.opponent.def)
 		);
 
-		const playerDmg = Math.floor(Math.random() * (dmg - acc + 1) + acc) + 1;
+		let playerDmg = Math.floor(Math.random() * (dmg - acc + 1) + acc) + 1;
 
-		const opponentSuccess = Math.random() < this.opponent.eva / 100;
+		const opponentSuccess =
+			Math.random() < (this.opponent.eva - this.opponent.weight) / 100;
 
 		if (opponentSuccess) {
-			this.opponent.hp -= playerDmg;
-			this.actions.push(
-				`${this.character.name} attacked ${this.opponent.name} and did ${playerDmg} damage!`
-			);
+			const crit = (this.character.crit + 1) / 201 / 100;
+
+			if (this.character.crit > 0 && Math.random() < crit) {
+				this.opponent.hp -= playerDmg + playerDmg;
+				this.actions.push(
+					`${this.character.name} attacked ${this.opponent.name} and did ${playerDmg} damage. Its a critical hit!`
+				);
+			} else {
+				this.opponent.hp -= playerDmg;
+				this.actions.push(
+					`${this.character.name} attacked ${this.opponent.name} and did ${playerDmg} damage!`
+				);
+			}
 		} else {
 			this.actions.push(`${this.opponent.name} avoided the attack!`);
 		}
@@ -276,13 +394,23 @@ class Battle {
 
 		const opponentDmg = Math.floor(Math.random() * (dmg - acc + 1) + acc) + 1;
 
-		const playerSuccess = Math.random() < this.character.eva / 100;
+		const playerSuccess =
+			Math.random() < (this.character.eva - this.character.weight) / 100;
 
 		if (playerSuccess) {
-			this.character.hp -= opponentDmg;
-			this.actions.push(
-				`${this.opponent.name} attacked ${this.character.name} and did ${opponentDmg} damage!`
-			);
+			const crit = (this.opponent.crit + 1) / 201 / 100;
+
+			if (this.opponent.crit > 0 && Math.random() < crit) {
+				this.character.hp -= opponentDmg + opponentDmg;
+				this.actions.push(
+					`${this.opponent.name} attacked ${this.character.name} and did ${opponentDmg} damage. Its a critical hit!`
+				);
+			} else {
+				this.character.hp -= opponentDmg;
+				this.actions.push(
+					`${this.opponent.name} attacked ${this.character.name} and did ${opponentDmg} damage!`
+				);
+			}
 		} else {
 			this.actions.push(`${this.character.name} avoided the attack!`);
 		}
@@ -307,9 +435,7 @@ class Battle {
 					name: `${this.character.name}`,
 					value: `**• ${constants.assets.xp.emoji} Level:** ${
 						this.character.level
-					}\n**• ${constants.assets.weapon.emoji} Weapon:** None\n**• ${
-						constants.assets.hp.emoji
-					} HP:** ${this.character.hp}\n**• ${
+					}\n**• ${constants.assets.hp.emoji} HP:** ${this.character.hp}\n**• ${
 						constants.assets.str.emoji
 					} STR:** ${this.character.str}\n**• ${
 						constants.assets.agl.emoji
@@ -319,7 +445,7 @@ class Battle {
 						constants.assets.acc.emoji
 					} ACC:** ${this.character.acc}\n**• ${
 						constants.assets.eva.emoji
-					} EVA:** ${this.character.sta}\n**• ${
+					} EVA:** ${this.character.eva}\n**• ${
 						constants.assets.eva.emoji
 					} ATT:** ${this.character.att}\n**• ${
 						constants.assets.def.emoji
@@ -334,9 +460,7 @@ class Battle {
 					name: `${this.opponent.name}`,
 					value: `**• ${constants.assets.xp.emoji} Level:** ${
 						this.opponent.level
-					}\n**• ${constants.assets.weapon.emoji} Weapon:** None\n**• ${
-						constants.assets.hp.emoji
-					} HP:** ${this.opponent.hp}\n**• ${
+					}\n**• ${constants.assets.hp.emoji} HP:** ${this.opponent.hp}\n**• ${
 						constants.assets.str.emoji
 					} STR:** ${this.opponent.str}\n**• ${
 						constants.assets.agl.emoji
