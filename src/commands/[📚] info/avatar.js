72f0e101116eacc7db90439bfbb6ab9e47eb6481
MiraBellerr <75858@siswa.unimas.my>
2022-08-06 03:00:56 +0800

    add get user avatar interaction

--- src/commands/[📚] info/avatar.js
+++ src/commands/[📚] info/avatar.js
@@ -1,4 +1,8 @@
-const { EmbedBuilder } = require("discord.js");
+const {
+	EmbedBuilder,
+	ContextMenuCommandBuilder,
+	ApplicationCommandType,
+} = require("discord.js");
 const {
 	getUserFromArguments,
 	getMemberFromArguments,
@@ -125,5 +129,125 @@ module.exports = {
 		pages.push(banner);
 
 		new Paginate.Paginate(client, message, pages).init();
+	},
+	interaction: {
+		data: new ContextMenuCommandBuilder()
+			.setName("Get User Avatar")
+			.setType(ApplicationCommandType.User),
+		run: async (client, interaction) => {
+			const pages = [];
+			const user = await client.users.fetch(interaction.targetId);
+
+			const embed = new EmbedBuilder()
+				.setAuthor({
+					name: interaction.user.username,
+					iconURL: interaction.user.displayAvatarURL(),
+				})
+				.setTitle(`${user.username}'s avatar`)
+				.setColor("#CD1C6C")
+				.setDescription(
+					`[webp](${user.displayAvatarURL({
+						size: 4096,
+						extension: "webp",
+					})}) | [png](${user.displayAvatarURL({
+						size: 4096,
+						extension: "png",
+					})}) | [jpg](${user.displayAvatarURL({
+						size: 4096,
+						extension: "jpg",
+					})}) | [jpeg](${user.displayAvatarURL({
+						size: 4096,
+						extension: "jpeg",
+					})}) | [gif](${user.displayAvatarURL({
+						size: 4096,
+						extension: "gif",
+					})})`
+				)
+				.setImage(user.displayAvatarURL({ size: 4096 }))
+				.setTimestamp()
+				.setFooter({
+					text: client.user.tag,
+					iconURL: client.user.displayAvatarURL(),
+				});
+
+			pages.push(embed);
+
+			const guild = await client.guilds.fetch(interaction.guildId);
+			const member = await guild.members.fetch(user);
+
+			if (member && member.avatarURL()) {
+				const embed = new EmbedBuilder()
+					.setAuthor({
+						name: interaction.user.username,
+						iconURL: interaction.user.displayAvatarURL(),
+					})
+					.setTitle(`${member.user.username}'s server avatar`)
+					.setColor("#CD1C6C")
+					.setDescription(
+						`[webp](${member.avatarURL({
+							size: 4096,
+							extension: "webp",
+						})}) | [png](${member.avatarURL({
+							size: 4096,
+							extension: "png",
+						})}) | [jpg](${member.avatarURL({
+							size: 4096,
+							extension: "jpg",
+						})}) | [jpeg](${member.avatarURL({
+							size: 4096,
+							extension: "jpeg",
+						})}) | [gif](${member.avatarURL({
+							size: 4096,
+							extension: "gif",
+						})})`
+					)
+					.setImage(member.avatarURL({ size: 4096 }))
+					.setTimestamp()
+					.setFooter({
+						text: client.user.tag,
+						iconURL: client.user.displayAvatarURL(),
+					});
+
+				pages.push(embed);
+			}
+
+			await user.fetch(true);
+
+			const banner = new EmbedBuilder()
+				.setAuthor({
+					name: interaction.user.username,
+					iconURL: interaction.user.displayAvatarURL(),
+				})
+				.setTitle(`${user.username}'s banner`)
+				.setColor("#CD1C6C")
+				.setDescription(
+					`[webp](${user.bannerURL({
+						size: 4096,
+						extension: "webp",
+					})}) | [png](${user.bannerURL({
+						size: 4096,
+						extension: "png",
+					})}) | [jpg](${user.bannerURL({
+						size: 4096,
+						extension: "jpg",
+					})}) | [jpeg](${user.bannerURL({
+						size: 4096,
+						extension: "jpeg",
+					})}) | [gif](${user.bannerURL({
+						size: 4096,
+						extension: "gif",
+					})})`
+				)
+				.setImage(user.bannerURL({ size: 4096 }))
+				.setTimestamp()
+				.setFooter({
+					text: client.user.tag,
+					iconURL: client.user.displayAvatarURL(),
+				});
+
+			pages.push(banner);
+
+			new Paginate.Paginate(client, interaction, pages).init();
+		},
 	},
 };
