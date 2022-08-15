e405c3ec4a3a4d9af6d363d22c0caa49b9135d85
MiraBellerr <75858@siswa.unimas.my>
2022-08-15 12:04:01 +0800

    edit shardstatus command

--- src/commands/[📚] info/shardstatus.js
+++ src/commands/[📚] info/shardstatus.js
@@ -1,42 +1,77 @@
 const Discord = require("discord.js");
 const utils = require("../../utils/utils");
-const signale = require('signale');
-
+const signale = require("signale");
 
 module.exports = {
 	name: "shardstatus",
 	aliases: ["shard"],
 	category: "[📚] info",
 	description: "Shows the shards status",
-	run: async (client, interaction) => {
-	
-     try {
-       
-          client.shard.broadcastEval(client => [client.shard.ids, client.ws.status, client.ws.ping, client.guilds.cache.size])
-    .then((results) =>{
-        const embed = new MessageEmbed()
-            .setColor("RANDOM")
-            .setTitle(`📡 Total shards: (${interaction.client.shard.count})`)
-            .setFooter({ text: 'Jasmine > shardstats'})
-            
-        
-        results.map((data) => {
-            embed.addFields({ name: `Shard ${data[0]}`, value: `**ID:** \`[ ${data[1]} ]\` | **Ping:** \`[ ${data[2]}ms ]\` | **Guilds:** \`[ ${data[3]} ]\``, inline: false})
-        });
-        interaction.reply({ embeds: [embed] });
-    })
-            
-        } catch (err) {
- //       interaction.reply(`There is an error. Please try again later.`);
-        signale.fatal(err)
-    }
-    
-    
+	run: async (client, message) => {
+		const shards = await client.shard.broadcastEval((client) => [
+			client.shard.ids,
+			client.ws.status,
+			client.ws.ping,
+			client.guilds.cache.size,
+		]);
+
+		const embed = new Discord.EmbedBuilder()
+			.setAuthor({
+				name: message.author.username,
+				iconURL: message.author.displayAvatarURL(),
+			})
+			.setColor("#DA70D6")
+			.setTitle(`📡 Total shards: (${client.shard.count})`)
+			.setFooter({
+				text: client.user.username,
+				iconURL: client.user.displayAvatarURL(),
+			});
+
+		shards.map((data) => {
+			embed.addFields({
+				name: `Shard ${data[0]}`,
+				value: `**ID:** \`[ ${data[1]} ]\` | **Ping:** \`[ ${data[2]}ms ]\` | **Guilds:** \`[ ${data[3]} ]\``,
+				inline: false,
+			});
+		});
+
+		message.reply({ embeds: [embed] });
+	},
+	interaction: {
+		data: {
+			name: "shardstatus",
+			description: "Shows the shards status",
+			type: 1,
+		},
+		run: async (client, interaction) => {
+			const shards = await client.shard.broadcastEval((client) => [
+				client.shard.ids,
+				client.ws.status,
+				client.ws.ping,
+				client.guilds.cache.size,
+			]);
+
+			const embed = new Discord.EmbedBuilder()
+				.setAuthor({
+					name: interaction.user.username,
+					iconURL: interaction.user.displayAvatarURL(),
+				})
+				.setColor("#DA70D6")
+				.setTitle(`📡 Total shards: (${client.shard.count})`)
+				.setFooter({
+					text: client.user.username,
+					iconURL: client.user.displayAvatarURL(),
+				});
+
+			shards.map((data) => {
+				embed.addFields({
+					name: `Shard ${data[0]}`,
+					value: `**ID:** \`[ ${data[1]} ]\` | **Ping:** \`[ ${data[2]}ms ]\` | **Guilds:** \`[ ${data[3]} ]\``,
+					inline: false,
+				});
+			});
+
+			interaction.reply({ embeds: [embed] });
+		},
 	},
 };
-
-
-// mira please stop using message events
-
-
-
