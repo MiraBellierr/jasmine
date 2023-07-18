f1fa9b62e8c559e08cd6f01635382ae1d12757fa
MiraBellierr <mira-chan1@outlook.com>
2023-07-18 20:43:41 +0800

    change selector

--- src/commands/utility/roast.js
+++ src/commands/utility/roast.js
@@ -1,5 +1,9 @@
 const puppeteer = require("puppeteer");
 
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 module.exports = {
   name: "roast",
   description: "test him",
@@ -27,9 +31,15 @@ module.exports = {
       // Navigate the page to a URL
       await page.goto("https://www.roastedby.ai/");
 
-      await page.type(".inputBox", reply.content);
-      await page.click(".bg-blue-500");
-      await page.waitForSelector("div.flex:nth-child(3) > div:nth-child(1)");
+      await page.type(
+        "div[class='inputBox border text-black rounded-lg py-2 px-3 w-3/4 mr-2 font-normal text-sm bg-transparent']",
+        reply.content
+      );
+      await page.click(
+        "button[class='bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors']"
+      );
+      await wait(3000);
+
       const aiMessages = await page.$$(".message-ai");
       const lastMessage = await aiMessages[aiMessages.length - 1].evaluate(
         (element) => element.textContent
